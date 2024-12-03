5ce812159f204ce6722e928a2a4c2c59894eaf6c
Christine <chanc245@newschool.edu>
2024-12-03 16:17:18 -0500

    Update arduino code, final version from Sara

--- arduino_code_archives/Gates_And_States/Gates_And_States.ino
+++ arduino_code_archives/Gates_And_States/Gates_And_States.ino
@@ -17,14 +17,14 @@ const int baseSpeed = 50;
 const int superSpeed = 500;
 
 // Define button pins, LEDs and script numbers
-const int buttonPins[] = { A0, A1, A2 };         // Button 1, 2, 3 pins
+const int buttonPins[] = { A1, A2, A0 };         // Button 1, 2, 3 pins
 bool buttonPressed[] = { false, false, false };  // Tracks if each button was pressed
-const int ledPins[] = { 12, 11, A3 };            // LED 1, 2, 3 pins
+const int ledPins[] = { A3, 11, 12 };            // LED 1, 2, 3 pins
 const int scripts[] = { 1, 2, 3 };               // Corresponding script numbers
 
 //audio timer
 unsigned long audioStartTime = 0;  // To store the start time
-const unsigned long audioEndTime[] = {145000, 145000, 145000};
+const unsigned long audioEndTime[] = { 153000, 227000, 295000 };
 
 // Create AccelStepper objects for each motor
 AccelStepper motor1(AccelStepper::DRIVER, stepPin1, dirPin1);
@@ -85,7 +85,7 @@ void loop() {
   gateInteractions();  //handles all interactions with gates before measurement
   // Serial.println("2");
   // delay(10);
-// Check for confirmation from ESP32
+  // Check for confirmation from ESP32
   if (Serial.available()) {
     String confirmation = Serial.readStringUntil('\n');
     if (confirmation == "ACK") {
@@ -156,13 +156,14 @@ void gateInteractions() {
 void handleMeasurement() {
   digitalWrite(buttonLED, LOW);
   Serial.println("handle measurement");
-  
+
   if (HGateDetected && !CnotGateDetected) {
     // 50% chance for motor1 to move to 0 or 180
     Serial.println("Qbit 1 stop");
     int currentPosition = motor1.currentPosition();
+    motor1.setCurrentPosition(currentPosition % 200);
     int targetPosition;
-    
+
     if (random(0, 2) == 0) {
       // Move to 0 if not already there
       targetPosition = 0;
@@ -170,12 +171,14 @@ void handleMeasurement() {
       // Move to 180 (assuming 200 steps)
       targetPosition = 100;
     }
-    
+
     motor1.moveTo(targetPosition);
-    Serial.println(targetPosition);
-    
+    Serial.print("current position");
+    Serial.println(currentPosition);
+
     // Continue running until the motor reaches the target position
     while (motor1.distanceToGo() != 0) {
+      Serial.println(motor1.distanceToGo());
       motor1.run();
     }
   } else if (HGateDetected && CnotGateDetected) {
@@ -183,22 +186,23 @@ void handleMeasurement() {
     Serial.println("Qbit 1 and 2 stop");
     int currentPosition1 = motor1.currentPosition();
     int currentPosition2 = motor2.currentPosition();
+    motor1.setCurrentPosition(currentPosition1 % 200);
+    motor2.setCurrentPosition(currentPosition2 % 200);
     int targetPosition;
-    
+
     if (random(0, 2) == 0) {
       targetPosition = 0;
     } else {
       targetPosition = 100;
     }
-    
+
     motor1.moveTo(targetPosition);
     motor2.moveTo(targetPosition);
-    
+
     // Continue running until both motors reach their target positions
     while (motor1.distanceToGo() != 0 || motor2.distanceToGo() != 0) {
       motor1.run();
       motor2.run();
-      
     }
   }
 }
@@ -218,12 +222,13 @@ void audioScriptControl(int buttonIndex) {
     // Debounce delay
     delay(50);
     if (digitalRead(buttonPins[buttonIndex]) == HIGH) {  // Confirm button is still pressed
-      buttonPressed[buttonIndex] = true;  // Mark button as pressed
+      buttonPressed[buttonIndex] = true;                 // Mark button as pressed
       digitalWrite(ledPins[buttonIndex], HIGH);
       Serial.println(scripts[buttonIndex]);  // Print the selected script
       Serial.println(scripts[buttonIndex]);
       Serial.println(scripts[buttonIndex]);
       Serial.println(scripts[buttonIndex]);
+      motor1.runSpeed();
       audioPlaying = true;
       audioStartTime = millis();
     }
@@ -236,14 +241,18 @@ void audioScriptControl(int buttonIndex) {
 }
 
 void audioTimer(int buttonIndex) {
-if (millis() - audioStartTime >= audioEndTime[buttonIndex]) {
-    audioPlaying=false;
+  if (millis() - audioStartTime >= audioEndTime[buttonIndex]) {
+    audioPlaying = false;
   }
 }
 
-void resetGates(){
+void resetGates() {
   bellState = false;
   measureButtonPressed = false;
+  motor1.setSpeed(baseSpeed);
+  motor2.setSpeed(baseSpeed);
+  motor1.runSpeed();
+  motor2.runSpeed();
   digitalWrite(buttonLED, LOW);
 }
 void resetExperience() {
